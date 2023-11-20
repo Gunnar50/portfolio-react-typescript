cbdfd6d5a041093721e1c8173c39165cc032aef3
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-20 10:00:49 +0000

    refractor

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,32 @@
+import aboutImage from "../assets/images/bg10.png";
 function About() {
 	return (
 		<section id="about">
-			<div className="about"></div>
+			<div
+				className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 
+							sm:py-30 md:py-24 md:flex-row md:space-x-4 md:text-left"
+			>
+				<div className="md:mt-2 md:w-3/5">
+					<h2 className="text-3xl font-bold mt-6 md:mt-0 md:text-4xl ">
+						About
+					</h2>
+					<p>
+						Hi, my name is Gustavo and I am a
+						<span className="font-bold"> highly ambitious</span>,
+						<span className="font-bold"> self-motivated</span>, and
+						<span className="font-bold"> driven</span> person based in London,
+						UK.
+					</p>
+				</div>
+				<div className="md:mt-2 md:w-1/2">
+					<img
+						src={aboutImage}
+						alt="Profile image"
+						width={320}
+						className="border-[6px] border-white rounded-[20px] rounded-tl-[110px]"
+					/>
+				</div>
+			</div>
 		</section>
 	);
 }
