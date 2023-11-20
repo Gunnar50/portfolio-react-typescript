d2bd2deed16b00662b2ddbd31dc77d662c98c4f6
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-20 10:00:51 +0000

    refractor

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,16 @@
 function Skills() {
 	return (
 		<section id="skills">
-			<div className="skills"></div>
+			<div>
+				<h2 className="text-3xl font-bold mt-6 md:mt-0 md:text-4xl ">Skills</h2>
+				<div>
+					<div className="border rounded-lg">
+						<div>
+							<img src="" /> <h3>Backend</h3>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
