a6132f4c49a549ba76f3e784b7d7a093153b1dc2
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-05 10:30:46 +0000

    removed qualifications for now

--- src/components/Qualification.tsx
+++ src/components/Qualification.tsx
@@ -1,31 +1,5 @@
-import { IconContext } from "react-icons";
-import { GiGraduateCap } from "react-icons/gi";
-
 function Qualification() {
-	return (
-		<section id="qualification">
-			<IconContext.Provider
-				value={{
-					size: "50px",
-					className: "text-background bg-neutral-primary rounded-full p-1",
-				}}
-			>
-				<div className="flex justify-center">
-					<div className="md:mt-2 md:w-1/4 ">
-						<h2>Academic Qualifications</h2>
-						<div className="flex">
-							<GiGraduateCap />
-						</div>
-					</div>
-					<div className="md:mt-2 md:w-1/4 flex justify-end">
-						<div className="">
-							<h2>Work Experience</h2>
-						</div>
-					</div>
-				</div>
-			</IconContext.Provider>
-		</section>
-	);
+	return <section id="qualification"></section>;
 }
 
 export default Qualification;
