3e7bd507a569bdc06181e679be2405aec9238983
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-10 16:25:51 +0000

    added toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Cursor from "./components/Cursor";
 import HomePage from "./pages/home";
 import "./styles/App.scss";
@@ -6,6 +8,7 @@ import "./styles/App.scss";
 function App() {
 	return (
 		<>
+			<ToastContainer />
 			<Cursor />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,56 +1,23 @@
 import axios from "axios";
-import { useState } from "react";
-import {
-	AiOutlineLinkedin,
-	AiOutlineMail,
-	AiOutlinePhone,
-	AiOutlineWhatsApp,
-} from "react-icons/ai";
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import Spinner from "../Spinner";
 import { Button } from "./button";
 import { Input } from "./input";
 import { Textarea } from "./textarea";
 
 const email = "gustavo.passarella@hotmail.com";
-const phoneNumber = "+447580577781";
-
-const contactItems = [
-	{
-		id: 0,
-		label: "Call",
-		link: `tel:${phoneNumber}`,
-		icon: AiOutlinePhone,
-	},
-	{
-		id: 1,
-		label: "Email",
-		link: `mailto:${email}`,
-		icon: AiOutlineMail,
-	},
-	{
-		id: 2,
-		label: "WhatsApp",
-		link: `https://wa.me/${phoneNumber}`,
-		icon: AiOutlineWhatsApp,
-	},
-	{
-		id: 3,
-		label: "LinkedIn",
-		link: `https://www.linkedin.com/in/gustavo-passarella/`,
-		icon: AiOutlineLinkedin,
-	},
-];
 
 function Contact() {
-	const [result, setResult] = useState("");
+	const [loading, setLoading] = useState(false);
 
-	const onSubmitForm = async (event) => {
+	const onSubmitForm = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		setResult("Sending....");
-		const formData = new FormData(event.target);
+		setLoading(true);
+		const formData = new FormData(event.target as HTMLFormElement);
 
 		formData.append("access_key", "bb4d7297-595d-4a43-bbe9-718d25d09d9d");
-		console.log(formData);
 
 		try {
 			const response = await axios.post(
@@ -62,24 +29,23 @@ function Contact() {
 					},
 				}
 			);
-			console.log(response);
 
 			if (response.data.success) {
-				console.log("Success", response);
-				setResult(response.data.message);
+				// console.log("Success", response);
+				toast.success(response.data.message);
 			} else {
-				console.log("Error", response);
-				setResult(response.data.message);
+				// console.log("Error", response);
+				toast.error(response.data.message);
 			}
 		} catch (error) {
 			console.error("An error occurred", error);
-			setResult("An error occurred. Please try again later.");
 		}
+		setLoading(false);
 	};
 	return (
 		<section id="contact">
 			<div
-				className="animate-fadeIn animation-delay-2  mx-auto p-6 md:px-10 py-14 max-w-3xl 
+				className="animate-fadeIn animation-delay-2 mx-auto p-6 md:px-10 py-14 max-w-4xl 
 							bg-gradient-to-tr from-[#0B1D2F] to-[#134E48]
 							rounded-tl-[20px] rounded-br-[20px] rounded-tr-[50px] rounded-bl-[50px] contact-shadow"
 			>
@@ -90,8 +56,8 @@ function Contact() {
 									md:mt-1 md:w-14 md:-translate-x-[65px]"
 					/>
 				</h2>
-				<div className="flex flex-col items-center md:flex-row md:items-start px-10">
-					<div className="md:w-3/5 mb-8 text-center md:text-left pr-10">
+				<div className="flex flex-col items-center md:flex-row md:items-start md:px-10">
+					<div className="md:w-2/5 mb-8 text-center md:text-left md:pr-10 md:mr-10">
 						<h3 className="text-2xl md:text-3xl font-semibold mb-4">
 							Available for freelance and fulltime opportunities
 						</h3>
@@ -113,7 +79,7 @@ function Contact() {
 						</p>
 					</div>
 
-					<div className="md:w-1/2  w-full">
+					<div className="md:w-3/5 w-full">
 						<form onSubmit={onSubmitForm} className="flex flex-col gap-2">
 							<input
 								type="hidden"
@@ -130,11 +96,10 @@ function Contact() {
 								required
 							></Textarea>
 
-							<Button variant="secondary" type="submit">
-								Submit
+							<Button variant="secondary" type="submit" className="relative">
+								Submit {loading && <Spinner />}
 							</Button>
 						</form>
-						<span>{result}</span>
 					</div>
 				</div>
 			</div>
